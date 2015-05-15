254e090b99d838b0eaafa7ff5cb94f1d7bc157e2
XuepengXu <xukobe@gmail.com>
2015-05-15 19:46:21 -0400

    demo3

--- DSRC_GUI/GUI_Extension/Demo3Start.py
+++ DSRC_GUI/GUI_Extension/Demo3Start.py
@@ -17,10 +17,28 @@ def execute(console):
         console.log("Demo3", "At least two cars are needed!")
         return
 
-    job = DSRC_Event.EventJob(DSRC_Event.ACTION_NAME_GO, 30, 0, 8)
-    msg = DSRC_Message_Coder.MessageCoder.generate_batch_processing(console.source, cars[0].name, job)
+    # job = DSRC_Event.EventJob(DSRC_Event.ACTION_NAME_GO, 30, 0, 8)
+    # msg = DSRC_Message_Coder.MessageCoder.generate_batch_processing(console.source, cars[0].name, job)
+    # console.send_msg(msg)
+    #
+    # job = DSRC_Event.EventJob(DSRC_Event.ACTION_NAME_GO, 30, 0, 8)
+    # msg = DSRC_Message_Coder.MessageCoder.generate_batch_processing(console.source, cars[1].name, job)
+    # console.send_msg(msg)
+
+    msg = generate_automove_message(console.source, cars[0].name, True)
     console.send_msg(msg)
 
-    job = DSRC_Event.EventJob(DSRC_Event.ACTION_NAME_GO, 30, 0, 8)
-    msg = DSRC_Message_Coder.MessageCoder.generate_batch_processing(console.source, cars[1].name, job)
+    msg = generate_automove_message(console.source, cars[1].name, True)
     console.send_msg(msg)
+
+
+
+def generate_automove_message(source, destination, automove):
+    msg_obj = {}
+    msg_obj[DSRC_Event.KEY_SOURCE] = source
+    msg_obj[DSRC_Event.KEY_DESTINATION] = destination
+    msg_obj[DSRC_Event.KEY_TYPE] = DSRC_Event.TYPE_CUSTOMIZED
+    msg_obj[DSRC_Event.KEY_SUBTYPE] = 'automove'
+    msg_obj["do"] = automove
+    # msg = MessageCoder.encode(msg_obj)
+    return msg_obj
